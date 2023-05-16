751b0cfdbe5d1b10f3e1595c050a4142e1fef6cc
Stas <stbukovskiy12@gmail.com>
2023-05-16 20:53:42 +0300

    Initial commit

---
+++ item-finder/src/main/java/com/readnshare/itemfinder/ItemFinderApplication.java
@@ -0,0 +1,13 @@
+package com.readnshare.itemfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemFinderApplication.class, args);
+    }
+
+}
