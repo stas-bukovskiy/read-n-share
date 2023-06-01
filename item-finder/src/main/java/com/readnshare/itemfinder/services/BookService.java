4c3cb018f6f97a79eff0bf3f7cae2c811b895345
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-06-01 20:41:20 +0300

    feat: add verify Google Book ids and IMDb ids methods

--- item-finder/src/main/java/com/readnshare/itemfinder/services/BookService.java
+++ item-finder/src/main/java/com/readnshare/itemfinder/services/BookService.java
@@ -3,12 +3,17 @@ package com.readnshare.itemfinder.services;
 import com.readnshare.itemfinder.domain.Book;
 import com.readnshare.itemfinder.googlebooks.domain.BookSearchData;
 import com.readnshare.itemfinder.googlebooks.services.GoogleBookFindService;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface BookService {
 
     Mono<BookSearchData> searchBookByExpression(String expression, GoogleBookFindService.BookSearchOrder searchOrder, int startIndex, int maxResults);
 
     Mono<Book> getBookByGoogleId(String googleBookId);
+
+    Flux<Book> getBookByGoogleIds(List<String> googleBookIds);
 
 }
--- item-finder/src/main/java/com/readnshare/itemfinder/services/MovieService.java
+++ item-finder/src/main/java/com/readnshare/itemfinder/services/MovieService.java
@@ -2,12 +2,18 @@ package com.readnshare.itemfinder.services;
 
 import com.readnshare.itemfinder.domain.Movie;
 import com.readnshare.itemfinder.imdb.domain.MovieSearchData;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface MovieService {
 
     Mono<MovieSearchData> searchMovieByExpression(String expression);
 
     Mono<Movie> getMovieByImdbId(String imdbId);
+
+    Flux<Movie> getMovieByImdbIds(List<String> imdbIds);
+
 
 }
