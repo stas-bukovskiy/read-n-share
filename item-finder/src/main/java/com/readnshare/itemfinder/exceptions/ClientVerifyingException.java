122e067c170536893e5f732e8c6601dbfa3cb32a
stas-bukovskiy <stbukovskiy12@gmail.com>
2023-06-05 15:51:59 +0300

    feat: secure REST with new user-service rebase on REST

--- item-finder/src/main/java/com/readnshare/itemfinder/exceptions/ClientVerifyingException.java
+++
@@ -1,7 +0,0 @@
-package com.readnshare.itemfinder.exceptions;
-
-public class ClientVerifyingException extends RuntimeException {
-    public ClientVerifyingException(String msg) {
-        super(msg);
-    }
-}
---
+++ item-finder/src/main/java/com/readnshare/itemfinder/exceptions/UserNotVerifiedException.java
@@ -0,0 +1,7 @@
+package com.readnshare.itemfinder.exceptions;
+
+public class UserNotVerifiedException extends RuntimeException {
+    public UserNotVerifiedException(String msg) {
+        super(msg);
+    }
+}
